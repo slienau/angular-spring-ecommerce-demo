749dec24ae9c9b95af80efe787ea9df339d1e5c1
Sebastian Lienau <16371421+slienau@users.noreply.github.com>
2023-11-10 12:09:35 +0100

    Add entities

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/dev/appexpert/demo/ecommerce/entity/ProductCategory.java
@@ -0,0 +1,23 @@
+package dev.appexpert.demo.ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "product_category")
+@Data
+public class ProductCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "category_name")
+    private String categoryName;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
+    private Set<Product> products;
+}
