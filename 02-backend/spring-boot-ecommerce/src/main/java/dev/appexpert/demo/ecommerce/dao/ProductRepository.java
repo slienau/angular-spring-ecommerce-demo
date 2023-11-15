91b4a751e2423ebbc179995db1fa34f471b00902
Sebastian Lienau <16371421+slienau@users.noreply.github.com>
2023-11-15 11:11:54 +0100

    Allow CrossOrigin from localhost

--- 02-backend/spring-boot-ecommerce/src/main/java/dev/appexpert/demo/ecommerce/dao/ProductRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/dev/appexpert/demo/ecommerce/dao/ProductRepository.java
@@ -2,7 +2,9 @@ package dev.appexpert.demo.ecommerce.dao;
 
 import dev.appexpert.demo.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
