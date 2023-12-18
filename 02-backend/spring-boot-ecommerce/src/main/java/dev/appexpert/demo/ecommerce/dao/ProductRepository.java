3d4157b2d8bcf3e03e9804d46bdcbd68430acfe4
Sebastian Lienau <16371421+slienau@users.noreply.github.com>
2023-12-18 12:36:33 +0100

    Add findByCategoryId to spring backend

--- 02-backend/spring-boot-ecommerce/src/main/java/dev/appexpert/demo/ecommerce/dao/ProductRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/dev/appexpert/demo/ecommerce/dao/ProductRepository.java
@@ -1,10 +1,18 @@
 package dev.appexpert.demo.ecommerce.dao;
 
 import dev.appexpert.demo.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
+
 
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
+    // Query method (spring will generate and execute the query automatically)
+    // will be exposed automatically
+    // /api/products/search/findByCategoryId?id=2
+    Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
 }
