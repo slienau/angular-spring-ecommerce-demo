7e69f43c1a3bb6a3c77247efa1ef9c548c824bb6
Sebastian Lienau <16371421+slienau@users.noreply.github.com>
2024-01-30 16:17:18 +0100

    Add StateRepository

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/dev/appexpert/demo/ecommerce/dao/StateRepository.java
@@ -0,0 +1,17 @@
+package dev.appexpert.demo.ecommerce.dao;
+
+import dev.appexpert.demo.ecommerce.entity.State;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import java.util.List;
+
+@CrossOrigin("http://localhost:4200")
+@RepositoryRestResource(collectionResourceRel = "states", path = "states")
+public interface StateRepository extends JpaRepository<State, Integer> {
+
+    List<State> findByCountryCode(@Param("code") String code);
+
+}
