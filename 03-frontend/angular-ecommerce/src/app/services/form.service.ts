fc8bdad188877fe5eba69b8206191e3a6a8adb7b
Sebastian Lienau <16371421+slienau@users.noreply.github.com>
2024-01-30 16:36:45 +0100

    Add Country and State classes, and update FormService to fetch countries and states

--- 03-frontend/angular-ecommerce/src/app/services/form.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/form.service.ts
@@ -1,11 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable, map, of } from 'rxjs';
+import { Country } from '../common/country';
+import { State } from '../common/state';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FormService {
-  constructor() {}
+  private countriesUrl = 'http://localhost:8080/api/countries';
+  private statesUrl = 'http://localhost:8080/api/states';
+
+  constructor(private httpClient: HttpClient) {}
 
   getCreditCardMonths(startMonth: number): Observable<number[]> {
     let data: number[] = [];
@@ -29,4 +35,30 @@ export class FormService {
 
     return of(data);
   }
+
+  getCountries(): Observable<Country[]> {
+    return this.httpClient
+      .get<GetResponseCountries>(this.countriesUrl)
+      .pipe(map((response) => response._embedded.countries));
+  }
+
+  getStates(countryCode: string): Observable<State[]> {
+    const searchUrl = `${this.statesUrl}/search/findByCountryCode?code=${countryCode}`;
+
+    return this.httpClient
+      .get<GetResponseStates>(searchUrl)
+      .pipe(map((response) => response._embedded.states));
+  }
+}
+
+interface GetResponseCountries {
+  _embedded: {
+    countries: Country[];
+  };
+}
+
+interface GetResponseStates {
+  _embedded: {
+    states: State[];
+  };
 }
