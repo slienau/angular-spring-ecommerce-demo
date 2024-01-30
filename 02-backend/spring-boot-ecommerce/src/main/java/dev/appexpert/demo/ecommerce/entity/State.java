71ed06e871fb7f73cb7feddd292381681ca8b0ab
Sebastian Lienau <16371421+slienau@users.noreply.github.com>
2024-01-30 15:34:33 +0100

    Add Country and State entities

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/dev/appexpert/demo/ecommerce/entity/State.java
@@ -0,0 +1,22 @@
+package dev.appexpert.demo.ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name="state")
+@Data
+public class State {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name="country_id", nullable = false)
+    private Country country;
+}
