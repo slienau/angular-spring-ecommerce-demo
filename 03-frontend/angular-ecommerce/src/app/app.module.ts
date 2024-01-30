9f7195da2b5a150b481c6fcb6d37af55e5efbed1
Sebastian Lienau <16371421+slienau@users.noreply.github.com>
2024-01-30 10:57:25 +0100

    add 'CartDetailsComponent'

--- 03-frontend/angular-ecommerce/src/app/app.module.ts
+++ 03-frontend/angular-ecommerce/src/app/app.module.ts
@@ -11,9 +11,11 @@ import { SearchComponent } from './components/search/search.component';
 import {ProductDetailsComponent} from './components/product-details/product-details.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CartStatusComponent } from './components/cart-status/cart-status.component';
+import { CartDetailsComponent } from './components/cart-details/cart-details.component';
 
 // the order is important! The most specific routes should be first
 const routes: Routes = [
+  {path: 'cart-details', component: CartDetailsComponent},
   {path: 'products/:id', component: ProductDetailsComponent},
   {path: 'search/:keyword', component: ProductListComponent},
   {path: 'category/:id/:name', component: ProductListComponent},
@@ -30,7 +32,8 @@ const routes: Routes = [
     ProductCategoryMenuComponent,
     SearchComponent,
     ProductDetailsComponent,
-    CartStatusComponent
+    CartStatusComponent,
+    CartDetailsComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
