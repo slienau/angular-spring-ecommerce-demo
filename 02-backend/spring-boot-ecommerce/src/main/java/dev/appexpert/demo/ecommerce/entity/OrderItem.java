c8bfccf7704da066bad6b0d3dfa6bddc6df92fdb
Sebastian Lienau <16371421+slienau@users.noreply.github.com>
2024-01-31 14:14:49 +0100

    Add order entities to backend

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/dev/appexpert/demo/ecommerce/entity/OrderItem.java
@@ -0,0 +1,30 @@
+package dev.appexpert.demo.ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name="order_item")
+@Data
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @Column(name = "unit_price")
+    private BigDecimal unitPrice;
+
+    @Column(name = "quantity")
+    private int quantity;
+
+    @Column(name = "product_id")
+    private Long productId;
+
+}
