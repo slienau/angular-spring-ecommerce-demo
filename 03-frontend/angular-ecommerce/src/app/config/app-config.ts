fd617fb8a3a57f393ac84da6e9b8caf81fef0133
Sebastian Lienau <16371421+slienau@users.noreply.github.com>
2024-02-01 15:44:44 +0100

    Add Okta configuration and dependencies

---
+++ 03-frontend/angular-ecommerce/src/app/config/app-config.ts
@@ -0,0 +1,8 @@
+export default {
+  oidc: {
+    clientId: '0oaeuwiyrc6dOyjrc5d7',
+    issuer: 'https://dev-86837606.okta.com/oauth2/default',
+    redirectUri: 'http://localhost:4200/login/callback',
+    scopes: ['openid', 'profile', 'email'],
+  },
+};
