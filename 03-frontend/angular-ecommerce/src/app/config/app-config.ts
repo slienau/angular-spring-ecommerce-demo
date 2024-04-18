832f7dc45b70686b1ced449f6b0f4f48700b65a2
Sebastian Lienau <16371421+slienau@users.noreply.github.com>
2024-04-18 12:31:03 +0200

    enable HTTPS

--- 03-frontend/angular-ecommerce/src/app/config/app-config.ts
+++ 03-frontend/angular-ecommerce/src/app/config/app-config.ts
@@ -2,7 +2,7 @@ export default {
   oidc: {
     clientId: '0oaeuwiyrc6dOyjrc5d7',
     issuer: 'https://dev-86837606.okta.com/oauth2/default',
-    redirectUri: 'http://localhost:4200/login/callback',
+    redirectUri: 'https://localhost:4200/login/callback',
     scopes: ['openid', 'profile', 'email'],
   },
 };
